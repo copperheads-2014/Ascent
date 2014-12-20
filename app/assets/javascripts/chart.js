2b433fb0776376069f1ce798da7bf673a7b83983
Thomas Lee <djvoa12@gmail.com>
2014-12-19 23:08:18 -0600

    edited charts js with chart characteristics

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,65 +1,94 @@
 var ready;
+var chart;
+var display;
 
 ready = function(){
-  new Highcharts.Chart({
+  display = $('#display p');
+
+  chart = new Highcharts.Chart({
     chart: {
+      backgroundColor: '#D1DBDD',
+      borderColor: "#FFFFFF",
+      borderWidth: 5,
+      borderRadius: 15,
       zoomType: 'x',
-      renderTo: 'chart'
+      renderTo: 'chart',
+      style: {
+        fontFamily: 'Audiowide',
+        fontSize: '12px'
+      }
     },
     title: {
-      text: 'Fuck you, Timmy.'
+      text: 'Space Trip',
+      style: { "color": 'black'}
     },
     subtitle: {
-      text: document.ontouchstart === undefined ?
-              'Entirely fuck you all.' :
-              'Pinch the chart to zoom in'
+      text: 'Pinch the chart to zoom in'
     },
     xAxis: {
-        type: 'datetime',
-        minRange: 60 * 4
+      type: 'time',
+      minRange: 60 * 4,
+      title: {text: 'Time'},
+      tickInterval: 4 * 3600 * 1000,
+
     },
     yAxis: {
-        title: {
-            text: 'Altitute'
-        }
-    },
-    legend: {
-        enabled: false,
-        backgroundColor: '#E6E6FA'
+      title: {text: 'Altitute'}
     },
     plotOptions: {
-        area: {
-            fillColor: {
-                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-                stops: [
-                    [0, Highcharts.getOptions().colors[5]],
-                    [1, Highcharts.Color(Highcharts.getOptions().colors[7]).setOpacity(0).get('rgba')]
-                ]
-            },
-            marker: {
-                radius: 2
-            },
-            lineWidth: 1,
-            states: {
-                hover: {
-                    lineWidth: 1
-                }
-            },
-            threshold: null
-        }
+      series: {
+        allowPointSelect: true,
+          point: {
+            events: {
+              select: function() {
+                display.html("hello");
+              }
+            }
+          },
+        animation: {duration: 3000}
+      },
+      area: {
+        fillColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+            stops: [
+                [0, Highcharts.getOptions().colors[5]],
+                [1, Highcharts.Color(Highcharts.getOptions().colors[8]).setOpacity(0).get('rgba')]
+            ]
+        },
+        marker: {
+          radius: 2
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1
+          }
+        },
+        threshold: null
+      }
     },
-    series: [{
-        type: 'area',
-        name: 'USD to EUR',
-        pointInterval: 24 * 3600 * 1000,
-        pointStart: Date.UTC(2006, 0, 1),
-        data: [
-            0.8446, 0.8445, 0.8444, 0.8451,    0.8418, 0.8264,    0.8258, 0.8232,    0.8233, 0.8258,
-            0.8283, 0.8278, 0.8256, 0.8292,    0.8239, 0.8239,    0.8245, 0.8265,    0.8261, 0.8269,
-            0.8273, 0.8244, 0.8244, 0.8172,    0.8139, 0.8146,    0.8164, 0.82,    0.8269, 0.8269
-        ]
-    }]
+    legend: {
+      enabled: false,
+      backgroundColor: '#E6E6FA'
+    },
+    click: function(e) {
+      console.log(
+      "hi"
+      )
+    }
   });
+
+  $("#button-play").click(function(){
+    chart.addSeries({
+      type: 'area',
+      name: 'Altitude',
+      pointInterval: 60 * 1000,
+      pointStart: 0,
+      data: [0.8446, 0.8445, 0.8444, 0.8451, 0.8418, 0.8264, 0.8258, 0.8232, 0.8233, 0.8258, 0.8283, 0.8278, 0.8256, 0.8292, 0.8239, 0.8239, 0.8245, 0.840, 0.844, 0.845],
+      color: '#E6E6FA'
+    });
+  });
+
 };
 
 $(document).ready(ready);
