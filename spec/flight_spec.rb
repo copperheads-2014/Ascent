b33d69f34b58950d905762d37f5e05047e43e34c
Apprentice <danielle.kienzle@gmail.com>
2014-12-29 12:56:42 -0600

    added time and distance flight specs

--- spec/flight_spec.rb
+++ spec/flight_spec.rb
@@ -1,19 +1,29 @@
 require "rails_helper"
 
 describe Flight do
-  let(:flight) { Flight.create! }
+  let(:flight) { Flight.create!(callsign: "TEST") }
   let!(:starting_point) { DataPoint.create!(
                           flight_id: flight.id,
                           data: {
                             "latitude" => 1,
-                            "longitude" => 2
+                            "longitude" => 2,
+                            "time" => "00:00:00",
+                            "altitude" => 0
+                            }
+                          )}
+  let!(:mid_point) { DataPoint.create!(
+                          flight_id: flight.id,
+                          data: {
+                            "altitude" => 100
                             }
                           )}
   let!(:ending_point) { DataPoint.create!(
                           flight_id: flight.id,
                           data: {
                             "latitude" => 3,
-                            "longitude" => 4
+                            "longitude" => 4,
+                            "time" => "10:12:34",
+                            "altitude" => 0
                             }
                           )}
 
@@ -38,6 +48,36 @@ describe Flight do
   describe "#distance_traveled" do
     it "returns the total distance traveled" do
       expect(flight.distance_traveled.round).to eq(314)
+    end
+  end
+
+  describe "#callsign" do
+    it "has a callsign" do
+      expect(flight.callsign).to eq("TEST")
+    end
+  end
+
+  describe "#start_time" do
+    it "returns the flight start time" do
+      expect(flight.start_time).to eq("00:00:00")
+    end
+  end
+
+  describe "#end_time" do
+    it "returns the flight end time" do
+      expect(flight.end_time).to eq("10:12:34")
+    end
+  end
+
+  describe "#max_altitude_data_point" do
+    it "returns the data point with the highest altitude" do
+      expect(flight.max_altitude_data_point).to eq(mid_point)
+    end
+  end
+
+  describe "#max_altitude" do
+    it "returns the maximum altitude reached during the flight" do
+      expect(flight.max_altitude).to eq(100)
     end
   end
 end
