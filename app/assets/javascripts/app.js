7322e4f4b7fcf5ce99bdbd0a5b9283fc007a8447
Apprentice <danielle.kienzle@gmail.com>
2015-01-02 13:00:41 -0600

    added play-pause toggle function and play function to coordinate all gauges and chart

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,13 @@ var $display;
 var index_of_digit;
 var flight_id;
 var flight_data;
+var seriesIndex = 0;
+
+
+var togglePlayPause = function() {
+  $("#button-play").toggle();
+  $("#button-pause").toggle();
+}
 
 var ready = function() {
 
@@ -17,7 +24,6 @@ var ready = function() {
 
   $display = $('#display')
 
-  index_of_digit = (document.URL.search(/\/\d/)) + 1;
   flight_id = window.location.pathname.split('/')[2];
 
   var request = $.ajax({
@@ -27,28 +33,40 @@ var ready = function() {
 
   request.done(function(response){
     flight_data = response;
-    // var duration = 100 * flight_data.length;
     loadChart(flight_data);
     loadAltimeter(flight_data[0]);
-    loadThermometer(flight_data[1]);
+    loadThermometer(flight_data[0]);
     // loadMap();
-    loadBarometer();
+    // loadBarometer();
   });
+
+  var play = function(interval) {
+    indexInterval = setInterval(function() {
+      seriesIndex++;
+      playChart();
+      playAltimeter();
+      playThermometer();
+      // playBarometer();
+      // playMap();
+    }, interval);
+  }
+
+  var pause = function() {
+    clearInterval(indexInterval);
+  }
 
   $("#button-play").click(function(){
+    togglePlayPause();
     $display.show('slide', {direction: 'left'}, 400);
-    loadChart([0,0])
-    playChart(1);
-    playAltimeter(1);
-    playThermometer(1);
-    playBarometer(1)
-    // playMap();
+    loadChart([0,0]);
+    play(1);
   });
+
+  $("#button-pause").click(function() {
+    pause();
+    togglePlayPause();
+  })
 }
-
-// $("#map_button").click(function(){
-
-// })
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
