8931a60c68a1e6c00d33880d0b7b41d9b835cce5
Thomas Lee <djvoa12@gmail.com>
2015-01-02 13:42:07 -0600

    edited barometer

--- app/assets/javascripts/gauge_pressure.js
+++ app/assets/javascripts/gauge_pressure.js
@@ -1,54 +1,71 @@
 var barometer;
 
-var loadBarometer = function(){
+var loadBarometer = function(seriesData){
   barometer = new Highcharts.Chart({
-    chart: {
-      type: 'solidgauge',
-      renderTo: 'gauge_3'
-    },
-    title: "Barometric Pressure",
-    pane: {
-      center: ['50%', '85%'],
-      size: '140%',
-      startAngle: -90,
-      endAngle: 90,
-      background: {
-        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
-        innerRadius: '60%',
-        outerRadius: '100%',
-        shape: 'arc'
-      }
-    },
-    tooltip: {
-      enabled: false
-    },
-    // the value axis
-    yAxis: {
-      stops: [
-        [0.1, '#55BF3B'], // green
-        [0.5, '#DDDF0D'], // yellow
-        [0.9, '#DF5353'] // red
-      ],
-      lineWidth: 0,
-      minorTickInterval: null,
-      tickPixelInterval: 400,
-      tickWidth: 0,
-      title: {
-        y: -70
+      chart: {
+        type: 'solidgauge',
+        renderTo: 'gauge_3',
+        plotBorderWidth: 0,
+        spacingTop: 0,
+        spacingLeft: 0,
+        spacingRight: 0,
+        spacingBottom: 0
       },
-      labels: {
-        y: 16
-      }
-    },
-    plotOptions: {
-      solidgauge: {
-        dataLabels: {
-          y: 5,
-          borderWidth: 0,
-          useHTML: true
+      title: {
+        text: 'Barometer'
+      },
+      pane: {
+        center: ['50%', '85%'],
+        size: '100%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+          backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+          innerRadius: '60%',
+          outerRadius: '100%',
+          shape: 'arc'
         }
-      }
-    }
+      },
+      tooltip: {
+        enabled: false
+      },
+      yAxis: {
+        stops: [
+          [0.5, '#DDDF0D'], // yellow
+          [0.9, '#55BF3B'], // green
+          [0.1, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickPixelInterval: 100,
+        tickWidth: 0,
+        labels: {
+          y: 16
+        },
+        min: 0,
+        max: 1100
+      },
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: 5,
+            borderWidth: 0,
+            useHTML: true
+          }
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Pressure',
+        data: [seriesData],
+        dataLabels: {
+          format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                   '<span style="font-size:14px;color:silver">mb</span></div>'
+        }
+      }]
   });
 };
 
@@ -58,7 +75,7 @@ var playBarometer = function(interval_time) {
     var point = barometer.series[0].points[0];
     seriesIndex++;
     if (seriesIndex < flight_data.length) {
-      point.update(flight_data[seriesIndex].temp);
+      point.update(flight_data[seriesIndex].pressure);
     }
   }, interval_time);
 };
