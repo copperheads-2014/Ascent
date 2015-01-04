b62f384a8d2796c18f332e7c7aee8050010fa4ac
Apprentice <danielle.kienzle@gmail.com>
2015-01-04 17:01:33 -0600

    added if else handling depending on if the params are a user id or username

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friendship][:friend_id])
+    if params[:friendship][:friend_id]
+      friend = User.find(params[:friendship][:friend_id])
+    else
+      friend = User.find_by(username: params[:friendship][:username])
+    end
+    @friendship = current_user.friendships.build(friend_id: friend.id)
     if @friendship.save
       flash[:notice] = "Added friend"
       redirect_to root_url
