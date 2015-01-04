315e768af989c1968cc604e7f3e0d266606c4f43
Cody Knauer <codyknauer@gmail.com>
2015-01-04 01:36:35 -0600

    minor refactor of javascript window resizing

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150104044628) do
-=======
-ActiveRecord::Schema.define(version: 20150103184345) do
->>>>>>> navbar friend add divs w/o styling
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
