9e0a417954a38fff6fd3c62a59846f8b312ee419
Mak Arnautovic <mak@what.cx>
2015-01-04 22:54:55 -0600

    comments display on page and look pretty bad

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   enum status: [ :flight, :data_point ]
-  belongs_to :user
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   belongs_to :flight
   belongs_to :data_point
 end
