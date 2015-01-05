5b59477a300e0ef08ad26b992ce986c2c12520bc
Apprentice <danielle.kienzle@gmail.com>
2015-01-05 14:56:07 -0600

    added avatar thumb size and default avatar url path

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,13 +20,19 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [200, 200]
   end
 
+  version :avatar_thumb do
+    process :resize_to_fit => [80, 80]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    # ActionController::Base.helpers.asset_path("fallback/astronaut.jpg")
+
+    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "astronaut.jpg"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
