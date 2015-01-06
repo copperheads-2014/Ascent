357f483793bedd5c767208a40cc190a0557d3b0c
Mak Arnautovic <mak@what.cx>
2015-01-05 23:39:44 -0600

    got rid of extra hash layer for params

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -3,7 +3,7 @@ class LaunchesController < ApplicationController
 		@launch = Launch.new
 	end
 	def create
-		Launch.create(user_id: params[:friends][:friend_id], flight_id: params[:flights][:flight_id])
+		Launch.create(user_id: params[:friend_id], flight_id: params[:flight_id])
 		redirect_to user_path(current_user), notice: "Successfully added your friend to the flight!"
 	end
 end
