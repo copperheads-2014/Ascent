847baeb75f759d78f97a46e379d82c83b9bdad74
Mak Arnautovic <mak@what.cx>
2015-01-06 10:22:07 -0600

    removed drop down, can add anyone to flights, autocomplete

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -3,7 +3,8 @@ class LaunchesController < ApplicationController
 		@launch = Launch.new
 	end
 	def create
-		Launch.create(user_id: params[:friend_id], flight_id: params[:flight_id])
+    user = User.find_by(username: params[:username])
+		Launch.create(user: user, flight_id: params[:flight_id])
 		redirect_to user_path(current_user), notice: "Successfully added your friend to the flight!"
 	end
 end
