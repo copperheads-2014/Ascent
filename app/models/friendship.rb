5a93e47dda1527c433a1880e71e7da021a80beb9
Mak Arnautovic <mak@what.cx>
2015-01-07 10:57:19 -0600

    friend requests update via ajax/jquery

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,12 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
+  scope :unapproved, -> { where(approved: false) }
+
+  def approve!
+    update(approved: true)
+  end
+
   def self.find_id(user, friend)
     @friendship = Friendship.find_by(user_id: friend.id, friend_id: user.id)
     @inverse = Friendship.find_by(user_id: user.id, friend_id: friend.id)
